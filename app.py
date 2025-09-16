4bbeef9da15f81c878546fc88c2b295219d5e33e
DaniloReis10 <daniloreis10@gmail.com>
2025-09-16 13:26:11 -0300

    adding test the services
    Signed-off-by: DaniloReis10 <daniloreis10@gmail.com>

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ def create_app():
     from services.health     import bp as health_bp
     from services.courses    import bp as courses_bp
     from services.years_suap import bp as years_suap_bp 
-    from services.studentbycities import bp as studentbycities_bp   
+    from services.studentbycities import bp as studentbycities_bp  
+    from services.enrollments import bp as enrollments_bp
+    from services.analysis   import bp as analysis_bp 
 
     app.register_blueprint(health_bp)      # /health
     app.register_blueprint(matriculas_bp)  # /matriculas
     app.register_blueprint(courses_bp)     # /cursos
     app.register_blueprint(years_suap_bp)  # /years_suap
     app.register_blueprint(studentbycities_bp)  # /studentbycities
+    app.register_blueprint(enrollments_bp) # /enrollments
+    app.register_blueprint(analysis_bp)    # /analysis
 
     return app
 
